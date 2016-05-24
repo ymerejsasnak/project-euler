8ad37f6f97c1436ba6d1027498ee808b5c5a09f5
Jeremy A Kansas <ymerejsasnak@gmail.com>
2016-05-24 14:56:54 -0400

    obvious issues remain, but does create new file...done enough for now

--- euler-getter.py
+++ euler-getter.py
@@ -6,13 +6,14 @@ and put it into a docstring in an empty appropriately named file
 
 import requests
 import sys
+import os.path
 
 
 problem_number = sys.argv[1]  # will only take a number, anything else dropped, nothing checked currently
 print(sys.argv, problem_number)
 
 
-r = requests.get('https://projecteuler.net/problem=' + str(problem_number))
+r = requests.get('https://projecteuler.net/problem=' + problem_number)
 
 # split on unique bit of text before problem text, and only keep second half
 problem_text = r.text.split('<div class="problem_content" role="problem">')[1]
@@ -20,7 +21,20 @@ problem_text = r.text.split('<div class="problem_content" role="problem">')[1]
 # do the same, split on the div closing tag and only keep what comes before
 problem_text = problem_text.split('</div>')[0]
 
+# remove paragraph tags
 problem_text = problem_text.replace('<p>', '').replace('</p>', '')
 
-print(problem_number, problem_text)
+# note some formatting tags (sup, sub, etc) will remain currently...but maybe that's ok for now
+# other note:  add newlines every 80 characters or so?
+
+
+
+filename = 'euler' + problem_number + '.py'
+
+if os.path.isfile(filename):
+    print('File already exists...exiting...')
+else:
+    with open(filename, 'w') as f:
+        f.write("'''" + problem_text + "'''")
+    print(filename + ' created.')
 
